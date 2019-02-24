5ba2791ba0e9d48df4078a2f0b22e0e688503485
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-24 17:20:40 +0300

    init

---
+++ food/generation.js
@@ -0,0 +1,3 @@
+var date = new Date();
+console.log('?v=' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear());
+console.log(`?v=${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`);
